7d6e258b8d51a00af3c6091aa70f796a72c4b4d8
brooke2384 <132254128+brooke2384@users.noreply.github.com>
2024-09-10 23:49:50 +0300

    updated ,missing last section

---
+++ script.js
@@ -0,0 +1,43 @@
+// Script for tab switching functionality
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (var tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (var tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Script for menu toggle functionality
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+// Script for Google Sheets form submission
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzNYcy16rO5gM4LTc2yZqgD4FvORt46ZA3Lj2MMJeAmYKa1jUXqOkqcDKZUhj1N7vhO-g/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message submitted";
+            setTimeout(function () {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message));
+});
